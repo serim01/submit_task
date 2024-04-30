93511ce0a39158e12b7bf990b55088fbec636b3e
serim_Kim <kkhksl13@gmail.com>
2024-04-30 16:47:12 +0900

    level1- 요구사항2

--- App.java
+++ App.java
@@ -8,5 +8,9 @@ public class App {
         int firstNum = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
         int secondNum = sc.nextInt();
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        sc.nextLine();
+        String operator = sc.nextLine();
+        char real_operator = operator.charAt(0);
     }
 }
