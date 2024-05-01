3eb85d74c291af5a093056d163839cbfbf23e048
serim_Kim <kkhksl13@gmail.com>
2024-05-01 11:18:09 +0900

    level2- 요구사항1

---
+++ BadInputException.java
@@ -0,0 +1,5 @@
+public class BadInputException extends Exception {
+    public BadInputException(String type) {
+        super("잘못된 입력입니다! " + type );
+    }
+}
---
+++ Calculator.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Calculator {
+    /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
+    ArrayList<String> resultList = new ArrayList<String>();
+
+    public int calculate(int firstNum, int secondNum, char operator) throws BadInputException{
+
+        if (operator == '/' && secondNum == 0){
+            throw new BadInputException("나눗셈에서 분모에 0이 들어올 수 없습니다.");
+        }
+        if (!(operator == '+' || operator == '-' || operator == '*' || operator == '/')){
+            throw  new BadInputException("사칙연산 기호를 정확히 입력해주세요");
+        }
+
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = firstNum + secondNum;
+                break;
+            case '-':
+                result = firstNum - secondNum;
+                break;
+            case '*':
+                result = firstNum * secondNum;
+                break;
+            case '/':
+                result = firstNum / secondNum;
+                break;
+        }
+
+        resultList.add(firstNum + " " + operator + " " + secondNum + " = " + result);
+
+        return result;
+    }
+}
